1a7594a7e13a4f644a94ea3d496f2753d62bfbe4
Glenn Posadas <hello@glennvon.com>
2024-02-09 16:51:37 +0800

    wrap home to statelessW

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,30 +1,41 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: const Text(
-          'Home'
-      ),
-      centerTitle: true,
-      backgroundColor: Colors.green[600],
-    ),
-    body: const Center(
-      child: Text(
-          'boom',
-        style: TextStyle(
+  home: Home()
+));
 
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+            'Home'
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.green[600],
+      ),
+      body: Center(
+        child: Text(
+          'Hey, world!',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+            fontFamily: 'IndieFlower',
+          ),
         ),
       ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {
-        print('Clicked!');
-      },
-      backgroundColor: Colors.red[600],
-      child: const Text(
-          'Click'
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('Clicked!');
+        },
+        backgroundColor: Colors.red[600],
+        child: const Text(
+            'G'
+        ),
       ),
-    ),
-  ),
-));
+    );
+  }
+}
