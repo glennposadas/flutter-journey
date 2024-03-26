4795098798a1e516ee7b3b65159d4634c1f8ca5d
Glenn Posadas <hello@glennvon.com>
2024-03-26 20:06:47 +0800

    add stateful stuff

--- ninja_id/lib/main.dart
+++ ninja_id/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_image_slideshow/flutter_image_slideshow.dart';
 
@@ -5,8 +6,16 @@ void main() => runApp(MaterialApp(
   home: NinjaCard(),
 ));
 
-class NinjaCard extends StatelessWidget {
+class NinjaCard extends StatefulWidget {
   const NinjaCard({super.key});
+
+  @override
+  State<NinjaCard> createState() => _NinjaCardState();
+}
+
+class _NinjaCardState extends State<NinjaCard> {
+
+  int ninjaLevel = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -21,14 +30,22 @@ class NinjaCard extends StatelessWidget {
         ),
       ),
       body: Padding(
-        padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+        padding: const EdgeInsets.fromLTRB(30, 40, 30, 0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/floki.png'),
-                radius: 50,
+            Center(
+              child: GestureDetector(
+                onTap: () {
+                  print("Clicked!");
+                  setState(() {
+                    ninjaLevel += 1;
+                  });
+                },
+                child: const CircleAvatar(
+                    backgroundImage: AssetImage('assets/floki.png'),
+                    radius: 50
+                ),
               )
             ),
             Divider(
@@ -67,9 +84,9 @@ class NinjaCard extends StatelessWidget {
             const SizedBox(
               height: 10,
             ),
-            const Text(
-              '99',
-              style: TextStyle(
+            Text(
+              '$ninjaLevel',
+              style: const TextStyle(
                   color: Colors.yellow,
                   letterSpacing: 2.0
               ),
