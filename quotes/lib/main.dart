38e5783e82da30db69fbb975cc4a77de6c24f25f
Glenn Posadas <hello@glennvon.com>
2024-03-29 18:08:31 +0800

    stateful quotelist app

--- quotes/lib/main.dart
+++ quotes/lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:quotes/quote.dart';
 
 void main() {
-  runApp(const MaterialApp(QuoteList));
+  runApp(
+      const MaterialApp(
+        home: QuoteList()
+      )
+  );
 }
 
 class QuoteList extends StatefulWidget {
@@ -12,8 +17,33 @@ class QuoteList extends StatefulWidget {
 }
 
 class _QuoteListState extends State<QuoteList> {
+
+  List<Quote> quotes = [
+    Quote(text: "Hello, world!", author: "Gavin"),
+    Quote(text: "Let's freakin' go!", author: "Adam"),
+    Quote(text: "I love you guys.", author: "Diana"),
+    Quote(text: "I'm tired", author: "Glenn")
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: const Text(
+            'Awesome Quotes',
+        ),
+        centerTitle: true,
+        titleTextStyle: const TextStyle(
+          color: Colors.white,
+          fontSize: 20
+        ),
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children:
+        quotes.map((quote) => Text("${quote.text} - ${quote.author}")).toList()
+      ),
+    );
   }
 }
