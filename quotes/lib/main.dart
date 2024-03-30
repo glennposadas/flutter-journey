bec4fc719f825786412ac02178e8fc6fc8a50efa
Glenn Posadas <hello@glennvon.com>
2024-03-30 11:23:47 +0800

    model

--- quotes/lib/main.dart
+++ quotes/lib/main.dart
@@ -18,8 +18,8 @@ class QuoteList extends StatefulWidget {
 
 class _QuoteListState extends State<QuoteList> {
 
-  List<Quote> quotes = [
-    Quote(text: "Hello, world!", author: "Gavin"),
+  List<Quote> quotes =
+    Quote(text: "Hello, world!", author: "Gavin."),
     Quote(text: "Let's freakin' go!", author: "Adam"),
     Quote(text: "I love you guys.", author: "Diana"),
     Quote(text: "I'm tired", author: "Glenn")
